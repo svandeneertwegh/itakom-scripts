04d710760b0e7d7e404e1b46d0794e31168c7e37
Stefan van den Eertwegh <stefan@corebyte.nl>
2025-11-14 12:25:10 +0100

    update

--- zabbix/install-ubuntu-proxy-sqlite.sh
+++ zabbix/install-ubuntu-proxy-sqlite.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ZABBIX_RELEASE_VERSION="7.4"
 UBUNTU_VERSION=$(lsb_release -rs)
@@ -7,22 +7,22 @@ ZABBIX_REPOSITORY_URL="https://repo.zabbix.com/zabbix/${ZABBIX_RELEASE_VERSION}/
 ZABBIX_APT_PACKAGE="zabbix-proxy-sqlite3"
 ZABBIX_PROXY_CONF="/etc/zabbix/zabbix_proxy.conf"
 
-echo -e "\033[0;32m-> \033[0m"
-echo -e "\033[0;32m-> You are installing zabbix proxy with sqlite3 support\033[0m"
-echo -e "\033[0;32m-> =================================================\033[0m"
-echo -e "\033[0;32m-> You are using Ubuntu version: ${UBUNTU_VERSION}\033[0m"
-echo -e "\033[0;32m-> You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}\033[0m"
-echo -e "\033[0;32m-> ================================================\033[0m"
+echo "\033[0;32m-> \033[0m"
+echo "\033[0;32m-> You are installing zabbix proxy with sqlite3 support\033[0m"
+echo "\033[0;32m-> =================================================\033[0m"
+echo "\033[0;32m-> You are using Ubuntu version: ${UBUNTU_VERSION}\033[0m"
+echo "\033[0;32m-> You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}\033[0m"
+echo "\033[0;32m-> ================================================\033[0m"
 
-echo -e "\033[0;32m-> Trying to install latest release\033[0m"
-echo -e "\033[0;32m-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository\033[0m"
+echo "\033[0;32m-> Trying to install latest release\033[0m"
+echo "\033[0;32m-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository\033[0m"
 sudo wget "${ZABBIX_REPOSITORY_URL}" >/dev/null 2>&1
-echo -e "\033[0;32m-> Install the zabbix source package\033[0m"
+echo "\033[0;32m-> Install the zabbix source package\033[0m"
 sudo dpkg -i "${PACKAGE_NAME}" >/dev/null 2>&1
-echo -e "\033[0;32m-> Remove obsolete zabbix source package\033[0m"
+echo "\033[0;32m-> Remove obsolete zabbix source package\033[0m"
 sudo rm "${PACKAGE_NAME}" >/dev/null 2>&1
 
-echo -e "\033[0;32m-> Update the apt package manager\033[0m"
+echo "\033[0;32m-> Update the apt package manager\033[0m"
 sudo apt update >/dev/null 2>&1
 
 if sudo dpkg -s "${ZABBIX_APT_PACKAGE}" | grep -q "install ok installed"; then
@@ -35,23 +35,23 @@ else
     sudo apt install "${ZABBIX_APT_PACKAGE}" -y
 fi
 
-echo -e "\033[0;32m-> Set hostname\033[0m"
+echo "\033[0;32m-> Set hostname\033[0m"
 read -p "Choose a hostname for zabbix proxy: " hostname
 read -p "Choose the zabbix server: " server
 
 sed -i 's/^Hostname=.*$/Hostname=$hostname/g' "${ZABBIX_PROXY_CONF}"
 sed -i 's/^Server=.*$/Server=$server/g' "${ZABBIX_PROXY_CONF}"
 
-echo -e "\033[0;32m-> Successfully installed ${ZABBIX_APT_PACKAGE}\033[0m"
-echo -e "\033[0;32m-> Successfully set hostname to '$hostname' in ${ZABBIX_PROXY_CONF}\033[0m"
-echo -e "\033[0;32m-> Successfully set server address to '$server' in ${ZABBIX_PROXY_CONF}\033[0m"
+echo "\033[0;32m-> Successfully installed ${ZABBIX_APT_PACKAGE}\033[0m"
+echo "\033[0;32m-> Successfully set hostname to '$hostname' in ${ZABBIX_PROXY_CONF}\033[0m"
+echo "\033[0;32m-> Successfully set server address to '$server' in ${ZABBIX_PROXY_CONF}\033[0m"
 
 sudo mkdir -p "/var/lib/zabbix-proxy"
 sudo chown -R zabbix:zabbix /var/lib/zabbix-proxy
-sed -i 's/^DBname=.*$/DBname=/var/lib/zabbix-proxy/database.sqlite3/g' "${ZABBIX_PROXY_CONF}"
+sed -i 's/^DBname=.*$/DBname=\/var\/lib\/zabbix-proxy\/database.sqlite3/g' "${ZABBIX_PROXY_CONF}"
 
-echo -e "\033[0;32m-> Successfully set sqlite3 database to /var/lib/zabbix-proxy/database.sqlite3\033[0m"
+echo "\033[0;32m-> Successfully set sqlite3 database to /var/lib/zabbix-proxy/database.sqlite3\033[0m"
 sudo systemctl enable zabbix-proxy >/dev/null 2>&1
 sudo systemctl restart zabbix-proxy >/dev/null 2>&1
-echo -e "\033[0;32m-> Successfully enabled autostart from boot and started it\033[0m"
-echo -e "\033[0;32m-> End script!\033[0m"
+echo "\033[0;32m-> Successfully enabled autostart from boot and started it\033[0m"
+echo "\033[0;32m-> End script!\033[0m"
