bab6b9d87a361d3f7e89811b5c252f6939e9ea72
Stefan van den Eertwegh <stefan@corebyte.nl>
2025-11-14 10:50:41 +0100

    update

--- zabbix/install-ubuntu-proxy-sqlite.sh
+++ zabbix/install-ubuntu-proxy-sqlite.sh
@@ -2,40 +2,40 @@
 
 ZABBIX_RELEASE_VERSION="7.4"
 UBUNTU_VERSION=$(lsb_release -rs)
+PACKAGE_NAME="zabbix-release_latest_${UBUNTU_VERSION}+ubuntu${UBUNTU_VERSION}_all.deb"
+ZABBIX_REPOSITORY_URL="https://repo.zabbix.com/zabbix/${UBUNTU_VERSION}/release/ubuntu/pool/main/z/zabbix-release/${PACKAGE_NAME}"
+APT_REPO_INSTALL_FLAG=0
 
-echo ""
-echo "You are installing zabbix proxy with sqlite3 support"
-echo "================================================="
-echo "You are using Ubuntu version: ${UBUNTU_VERSION}"
-echo "You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}"
-echo "================================================"
+echo "-> "
+echo "-> You are installing zabbix proxy with sqlite3 support"
+echo "-> ================================================="
+echo "-> You are using Ubuntu version: ${UBUNTU_VERSION}"
+echo "-> You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}"
+echo "-> ================================================"
 
-ZABBIX_RELEASE_DEB="zabbix-release_latest_${ZABBIX_RELEASE_VERSION}+ubuntu${UBUNTU_VERSION}_all.deb"
-ZABBIX_REPOSITORY_URL="https://repo.zabbix.com/zabbix/${UBUNTU_VERSION}/release/ubuntu/pool/main/z/zabbix-release/${ZABBIX_RELEASE_DEB}"
+if dpkg-query -l "$PACKAGE_NAME" >/dev/null 2>&1; then
+    echo "-> Package '$PACKAGE_NAME' is already installed."
+    echo "-> Latest zabbix release is already installed"
+    exit
+else
+    echo "-> Package '$PACKAGE_NAME' is NOT installed."
+    echo "-> Trying to install latest release"
+    echo "-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository"
+    sudo wget ${ZABBIX_REPOSITORY_URL}
+    echo "-> Install the zabbix source package"
+    sudo dpkg -i $ZABBIX_RELEASE_DEB
+    echo "-> Remove obsolete zabbix source package"
+    sudo rm $ZABBIX_RELEASE_DEB
+fi 
 
-echo "Download zabbix version ${ZABBIX_RELEASE_VERSION} apt repository"
 
-echo ${ZABBIX_REPOSITORY_URL}
-
-exit;
-
-sudo wget ${ZABBIX_REPOSITORY_URL}
-
-echo "Install the zabbix source package"
-sudo dpkg -i $ZABBIX_RELEASE_DEB
-
-echo "Remove obsolete zabbix source package"
-sudo rm $ZABBIX_RELEASE_DEB
-
-echo "Update the apt package manager"
+echo "-> Update the apt package manager"
 sudo apt update
 
-echo "Install zabbix proxy with sqlite support"
+echo "-> Install zabbix proxy with sqlite support"
 sudo apt install zabbix-proxy-sqlite3
 
-rm $ZABBIX_RELEASE_DEB
-
-echo "Set hostname"
+echo "-> Set hostname"
 read -p "Choose a hostname for zabbix proxy: " yn
 echo $yn
 
