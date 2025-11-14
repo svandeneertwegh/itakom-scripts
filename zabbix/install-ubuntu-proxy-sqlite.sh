8fdfe6c081461011770a999f19ffabba4767c801
Stefan van den Eertwegh <stefan@corebyte.nl>
2025-11-14 11:20:37 +0100

    update

--- zabbix/install-ubuntu-proxy-sqlite.sh
+++ zabbix/install-ubuntu-proxy-sqlite.sh
@@ -2,6 +2,7 @@
 
 ZABBIX_RELEASE_VERSION="7.4"
 UBUNTU_VERSION=$(lsb_release -rs)
+PACKAGE_NAME_SHORT="zabbix-release_latest_${ZABBIX_RELEASE_VERSION}+ubuntu${UBUNTU_VERSION}_all"
 PACKAGE_NAME="zabbix-release_latest_${ZABBIX_RELEASE_VERSION}+ubuntu${UBUNTU_VERSION}_all.deb"
 ZABBIX_REPOSITORY_URL="https://repo.zabbix.com/zabbix/${ZABBIX_RELEASE_VERSION}/release/ubuntu/pool/main/z/zabbix-release/${PACKAGE_NAME}"
 DEST_PATH="/tmp/${PACKAGE_NAME}"
@@ -15,12 +16,12 @@ echo -e "\033[0;32m-> You are using Ubuntu version: ${UBUNTU_VERSION}\033[0m"
 echo -e "\033[0;32m-> You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}\033[0m"
 echo -e "\033[0;32m-> ================================================\033[0m"
 
-if dpkg-query -l "$PACKAGE_NAME" >/dev/null 2>&1; then
-    echo -e "\033[0;32m-> Package '$PACKAGE_NAME' is already installed.\033[0m"
+if dpkg-query -l "$PACKAGE_NAME_SHORT" >/dev/null 2>&1; then
+    echo -e "\033[0;32m-> Package '$PACKAGE_NAME_SHORT' is already installed.\033[0m"
     echo -e "\033[0;32m-> Latest zabbix release is already installed\033[0m"
     exit
 else
-    echo -e "\033[0;32m-> Package '$PACKAGE_NAME' is NOT installed.\033[0m"
+    echo -e "\033[0;32m-> Package '$PACKAGE_NAME_SHORT' is NOT installed.\033[0m"
     echo -e "\033[0;32m-> Trying to install latest release\033[0m"
     echo -e "\033[0;32m-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository\033[0m"
     sudo wget -O - "$ZABBIX_REPOSITORY_URL" | sudo tee "$DEST_PATH" > /dev/null
