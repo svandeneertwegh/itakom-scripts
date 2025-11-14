56ed6f6f1a51e02960bf7afc3a50cc2e59d4f5c3
Stefan van den Eertwegh <stefan@corebyte.nl>
2025-11-14 11:18:38 +0100

    update

--- zabbix/install-ubuntu-proxy-sqlite.sh
+++ zabbix/install-ubuntu-proxy-sqlite.sh
@@ -5,6 +5,8 @@ UBUNTU_VERSION=$(lsb_release -rs)
 PACKAGE_NAME="zabbix-release_latest_${ZABBIX_RELEASE_VERSION}+ubuntu${UBUNTU_VERSION}_all.deb"
 ZABBIX_REPOSITORY_URL="https://repo.zabbix.com/zabbix/${ZABBIX_RELEASE_VERSION}/release/ubuntu/pool/main/z/zabbix-release/${PACKAGE_NAME}"
 DEST_PATH="/tmp/${PACKAGE_NAME}"
+ZABBIX_APT_PACKAGE="zabbix-proxy-sqlite3"
+ZABBIX_PROXY_CONF="/etc/zabbix/zabbix_proxy.conf"
 
 echo -e "\033[0;32m-> \033[0m"
 echo -e "\033[0;32m-> You are installing zabbix proxy with sqlite3 support\033[0m"
@@ -21,7 +23,6 @@ else
     echo -e "\033[0;32m-> Package '$PACKAGE_NAME' is NOT installed.\033[0m"
     echo -e "\033[0;32m-> Trying to install latest release\033[0m"
     echo -e "\033[0;32m-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository\033[0m"
-    echo $ZABBIX_REPOSITORY_URL;
     sudo wget -O - "$ZABBIX_REPOSITORY_URL" | sudo tee "$DEST_PATH" > /dev/null
     echo -e "\033[0;32m-> Install the zabbix source package\033[0m"
     sudo dpkg -i "${DEST_PATH}"
@@ -33,12 +34,21 @@ fi
 echo -e "\033[0;32m-> Update the apt package manager\033[0m"
 sudo apt update
 
-echo -e "\033[0;32m-> Install zabbix proxy with sqlite support\033[0m"
-sudo apt install zabbix-proxy-sqlite3
+if dpkg-query -l "$ZABBIX_APT_PACKAGE" >/dev/null 2>&1; then
+    echo "\033[0;32mPackage '$ZABBIX_APT_PACKAGE' is already installed.\033[0m"
+    exit;
+
+else
+    echo "\033[0;32mPackage '$ZABBIX_APT_PACKAGE' is NOT installed.\033[0m"
+    echo "\033[0;32mInstalling ${ZABBIX_APT_PACKAGE}\033[0m"
+    sudo apt install "${ZABBIX_APT_PACKAGE}" -y
+fi
 
 echo -e "\033[0;32m-> Set hostname\033[0m"
-read -p "Choose a hostname for zabbix proxy: " yn
+read -p "Choose a hostname for zabbix proxy: \n" yn
 echo $yn
 
-sed -i 's/DBName=(.*)/DBName=$yn/g' /etc/zabbix/zabbix-proxy
+sed -i 's/DBName=(.*)/DBName=$yn/g' "${ZABBIX_PROXY_CONF}"
 
+echo -e "\033[0;32m-> Successfully installed ${ZABBIX_APT_PACKAGE}\033[0m"
+echo -e "\033[0;32m-> Successfully set hostname in ${ZABBIX_PROXY_CONF}\033[0m"
