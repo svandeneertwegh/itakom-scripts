04abfb0985e2c329f2b2111428f5457b2a89284a
Stefan van den Eertwegh <stefan@corebyte.nl>
2025-11-14 14:13:59 +0100

    update

--- zabbix/install-ubuntu-proxy-sqlite.sh
+++ zabbix/install-ubuntu-proxy-sqlite.sh
@@ -7,42 +7,42 @@ ZABBIX_REPOSITORY_URL="https://repo.zabbix.com/zabbix/${ZABBIX_RELEASE_VERSION}/
 ZABBIX_APT_PACKAGE="zabbix-proxy-sqlite3"
 ZABBIX_PROXY_CONF="/etc/zabbix/zabbix_proxy.conf"
 
-echo "\033[0;32mStart install script\033[0m"
-echo "\033[0;32m-> \033[0m"
-echo "\033[0;32m-> You are installing zabbix proxy with sqlite3 support\033[0m"
-echo "\033[0;32m-> =================================================\033[0m"
-echo "\033[0;32m-> You are using Ubuntu version: ${UBUNTU_VERSION}\033[0m"
-echo "\033[0;32m-> You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}\033[0m"
-echo "\033[0;32m-> ================================================\033[0m"
-echo "\033[0;32m-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository\033[0m"
+echo "Start install script"
+echo "-> "
+echo "-> You are installing zabbix proxy with sqlite3 support"
+echo "-> ================================================="
+echo "-> You are using Ubuntu version: ${UBUNTU_VERSION}"
+echo "-> You are installing the zabbix release: ${ZABBIX_RELEASE_VERSION}"
+echo "-> ================================================"
+echo "-> Downloading zabbix version ${ZABBIX_RELEASE_VERSION} apt repository"
 sudo wget "${ZABBIX_REPOSITORY_URL}" >/dev/null 2>&1
 echo "done"
-echo "\033[0;32m-> Installing the zabbix apt repository package\033[0m"
+echo "-> Installing the zabbix apt repository package"
 sudo dpkg -i "${PACKAGE_NAME}" >/dev/null 2>&1
 echo "done"
 sudo rm "${PACKAGE_NAME}" >/dev/null 2>&1
 
-echo "\033[0;32m-> Update the apt package manager\033[0m"
+echo "-> Update the apt package manager"
 sudo apt update >/dev/null 2>&1
 echo "done"
 if sudo dpkg -s "${ZABBIX_APT_PACKAGE}" | grep -q "install ok installed"; then
-    echo "\033[0;32m-> Package '$ZABBIX_APT_PACKAGE' is already installed.\033[0m"
+    echo "-> Package '$ZABBIX_APT_PACKAGE' is already installed."
     read -p "Try to upgrade the '$ZABBIX_APT_PACKAGE' package? [y/n]: " try
     if [ "$try" = "y" ]; then
       if sudo apt list --upgradable 2>/dev/null | grep -q "$ZABBIX_APT_PACKAGE"; then
           sudo apt upgrade "$ZABBIX_APT_PACKAGE" >/dev/null 2>&1
-          echo "\033[0;32m-> Package '$ZABBIX_APT_PACKAGE' is succesfull upgraded!\033[0m"
+          echo "-> Package '$ZABBIX_APT_PACKAGE' is succesfully upgraded!"
           sudo apt systemctl restart zabbix-proxy
-          echo "\033[0;32m-> Successfully restarted zabbix-proxy!\033[0m"
+          echo "-> Successfully restarted zabbix-proxy!"
         else
-           echo "\033[0;32m-> No upgrade available!\033[0m"
+           echo "-> No upgrade available!"
         fi
     fi
     exit;
 
 else
-    echo "\033[0;32m-> Package '$ZABBIX_APT_PACKAGE' is NOT installed.\033[0m"
-    echo "\033[0;32m-> Installing ${ZABBIX_APT_PACKAGE}\033[0m"
+    echo "-> Package '$ZABBIX_APT_PACKAGE' is NOT installed."
+    echo "-> Installing ${ZABBIX_APT_PACKAGE}"
     sudo apt install "${ZABBIX_APT_PACKAGE}" -y > /dev/null 2>&1
     echo "done"
 fi
@@ -51,29 +51,29 @@ read -p "Configure zabbix-proxy? [y/n]: " answer
 
 if [ "$answer" = "y" ]; then
 
-    echo "\033[0;32m-> Set hostname\033[0m"
+    echo "-> Set hostname"
     read -p "Choose a hostname for zabbix proxy: " hostname
     read -p "Choose the zabbix server: " server
 
     sudo sed -i "s/^Hostname=.*$/Hostname=$hostname/g" "${ZABBIX_PROXY_CONF}"
     sudo sed -i "s/^Server=.*$/Server=$server/g" "${ZABBIX_PROXY_CONF}"
 
-    echo "\033[0;32m-> Successfully installed ${ZABBIX_APT_PACKAGE}\033[0m"
-    echo "\033[0;32m-> Successfully set hostname to '$hostname' in ${ZABBIX_PROXY_CONF}\033[0m"
-    echo "\033[0;32m-> Successfully set server address to '$server' in ${ZABBIX_PROXY_CONF}\033[0m"
+    echo "-> Successfully installed ${ZABBIX_APT_PACKAGE}"
+    echo "-> Successfully set hostname to '$hostname' in ${ZABBIX_PROXY_CONF}"
+    echo "-> Successfully set server address to '$server' in ${ZABBIX_PROXY_CONF}"
 
     sudo mkdir -p "/var/lib/zabbix-proxy"
-    echo "\033[0;32m-> Successfully made folder '/var/lib/zabbix-proxy'\033[0m"
+    echo "-> Successfully made folder '/var/lib/zabbix-proxy'"
     sudo chown -R zabbix:zabbix /var/lib/zabbix-proxy
-    echo "\033[0;32m-> Successfully set owner of folder to zabbix\033[0m"
+    echo "-> Successfully set owner of folder to zabbix"
 
     sudo sed -i 's/^DBname=.*$/DBname=\/var\/lib\/zabbix-proxy\/database.sqlite3/g' "${ZABBIX_PROXY_CONF}"
 
-    echo "\033[0;32m-> Successfully set sqlite3 database to /var/lib/zabbix-proxy/database.sqlite3\033[0m"
+    echo "-> Successfully set sqlite3 database to /var/lib/zabbix-proxy/database.sqlite3"
     sudo systemctl enable zabbix-proxy >/dev/null 2>&1
     sudo systemctl restart zabbix-proxy >/dev/null 2>&1
-    echo "\033[0;32m-> Successfully enabled autostart from boot and started it\033[0m"
+    echo "-> Successfully enabled autostart from boot and started it"
 else
   echo ""
 fi
-echo "\033[0;32m-> End script!\033[0m"
+echo "-> End script!"
