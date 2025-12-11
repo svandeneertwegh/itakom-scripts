607bfd815080ed94ec6b3ef1aa15455b578d88d6
Stefan van den Eertwegh <stefan@corebyte.nl>
2025-12-11 12:32:57 +0100

    update

--- zabbix/install-ubuntu-mibs-proxy.sh
+++ zabbix/install-ubuntu-mibs-proxy.sh
@@ -4,7 +4,6 @@ UBUNTU_VERSION=$(lsb_release -rs)
 SNMP_APT_PACKAGE="snmp snmpd snmp-mibs-downloader"
 UBUNTU_SNMP_MIBS_PATH="/usr/share/snmp/mibs/"
 UBUNTU_SNMP_CONF="/etc/snmp/snmp.conf"
-ZABBIX_PROXY_CONF="/etc/zabbix/zabbix_proxy.conf"
 
 echo "Start install custom mibs on ubuntu with zabbix-proxy"
 echo "-> "
@@ -19,10 +18,10 @@ sudo apt install "${SNMP_APT_PACKAGE}" -y > /dev/null 2>&1
 echo "done"
 echo "Location of custom-mibs on this system is:"
 echo "${UBUNTU_SNMP_MIBS_PATH}"
-echo "Now copy the mibs to the system location"
+echo "Now copying the custom mibs to the above path"
 sudo cp -rf custom-mibs/*.txt ${UBUNTU_SNMP_MIBS_PATH}
 echo "-> Remove the disabled snmp mibs loader"
-sudo sed -i "/s/^mibs :/# mibs :/g" "${UBUNTU_SNMP_CONF}"
+sudo sed -i "s/^mibs/# mibs/g" "${UBUNTU_SNMP_CONF}"
 echo "Enabled snmp loader"
 echo "Restarting proxy and snmp service"
 sudo systemctl restart zabbix-proxy snmpd
